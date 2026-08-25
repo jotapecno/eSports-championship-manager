12c55b269ff8c9cf54df8daa0adeba1ac6b5fb91
Vitor Hugo <vitorhugodasilvaoliveira80@gmail.com>
2026-08-25 13:47:29 -0300

    Update jogador.sql

--- database/tabelas/jogador.sql
+++ database/tabelas/jogador.sql
@@ -1,8 +1,8 @@
 CREATE TABLE jogador(
   id_jogador SERIAL PRIMARY KEY,
-  nome NOT NULL VARCHAR(50),
-  data_nascimento NOT NULL DATE,
-  endereco NOT NULL VARCHAR(100),
+  nome VARCHAR(50) NOT NULL,
+  data_nascimento DATE NOT NULL,
+  endereco VARCHAR(100) NOT NULL,
   numero_mpvs INTEGER,
   posicao_ranking INTEGER
 );
