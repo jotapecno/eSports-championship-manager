8664700a3f16ffaefbe7090030c21822da23117a
Matheus Carvalho <157723585+Matheus-Barbosa01@users.noreply.github.com>
2026-08-25 22:07:12 -0300

    Criação da tabela equipe no compeonato

---
+++ database/tabelas/equipe_campeonato.sql
@@ -0,0 +1,13 @@
+--  ENUM para o status de participação para enviar erros de digitação
+CREATE TYPE status_participacao AS ENUM ('Inscrita', 'Ativa', 'Eliminada', 'Desistente');
+
+CREATE TABLE equipe_campeonato (
+    id SERIAL PRIMARY KEY,
+    campeonato_id INT NOT NULL,
+    equipe_id INT NOT NULL,
+    data_inscricao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    status_participacao status_participacao_enum DEFAULT 'Inscrita',
+    
+    -- Restrição para evitar que a mesma equipe seja cadastrada duas vezes no mesmo campeonato
+    CONSTRAINT uk_equipe_campeonato UNIQUE (campeonato_id, equipe_id)
+);
