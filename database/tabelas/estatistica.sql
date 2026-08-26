82fd34444b340f9b561dee684ba5c4518bf70840
agsoo-arch <asafeagson@gmail.com>
2026-08-25 21:37:09 -0300

    criação da tabela estatistica

---
+++ database/tabelas/estatistica.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Estatistica (
+    id_estatistica INT AUTO_INCREMENT PRIMARY KEY,
+    id_partida INT NOT NULL,
+    id_equipe INT NOT NULL,
+    id_jogador INT,
+    nome_metrica VARCHAR(100) NOT NULL,
+    valor DECIMAL(10,2) NOT NULL,
+    CONSTRAINT fk_estatistica_partida FOREIGN KEY (id_partida) REFERENCES Partida(id_partida),
+    CONSTRAINT fk_estatistica_equipe FOREIGN KEY (id_equipe) REFERENCES Equipe(id_equipe),
+    CONSTRAINT fk_estatistica_jogador FOREIGN KEY (id_jogador) REFERENCES Jogador(id_jogador)
+);
