b156c1f6582b18c5edd03bf980532c226ca28203
Vitor Hugo <vitorhugodasilvaoliveira80@gmail.com>
2026-08-30 18:22:54 -0300

    Refactor jogador table structure and data types

--- database/tabelas/jogador.sql
+++ database/tabelas/jogador.sql
@@ -1,8 +1,9 @@
 CREATE TABLE jogador(
-  id_jogador SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   nome VARCHAR(50) NOT NULL,
+  cpf VARCHAR(11) NOT NULL,
   data_nascimento DATE NOT NULL,
   endereco VARCHAR(100) NOT NULL,
-  numero_mpvs INTEGER,
-  posicao_ranking INTEGER
+  numero_mpvs INT,
+  posicao_ranking INT
 );
