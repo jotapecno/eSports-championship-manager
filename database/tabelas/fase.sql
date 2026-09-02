feaa02679e34df35cb0497541e6ac6d835f5c1e9
Davi Mercês <chiasilv49@gmail.com>
2026-09-01 22:28:23 -0300

    Rename id_fase to id in fase table

--- database/tabelas/fase.sql
+++ database/tabelas/fase.sql
@@ -1,8 +1,8 @@
 CREATE TABLE fase (
-    id_fase SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     id_campeonato INT NOT NULL,
     nome_fase VARCHAR(50) NOT NULL,
     formato_disputa VARCHAR(50),
     data_inicio DATE,
-    data_fim DATE,
+    data_fim DATE
 );
