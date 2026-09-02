3d41c0e2260a47e96c7bc75ec5778ad101fb555c
Davi Mercês <chiasilv49@gmail.com>
2026-09-01 22:30:06 -0300

    Rename id_organizacao to id in organizacao table

--- database/tabelas/organização.sql
+++ database/tabelas/organização.sql
@@ -1,5 +1,5 @@
 CREATE TABLE organizacao (
-    id_organizacao SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     cnpj VARCHAR(18) UNIQUE,
     email_contato VARCHAR(100),
