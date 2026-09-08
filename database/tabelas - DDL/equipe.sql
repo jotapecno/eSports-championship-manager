2898b25de3c6d53fb716537ab0ad6c62792055cc
Vitor Hugo <vitorhugodasilvaoliveira80@gmail.com>
2026-09-08 11:50:12 -0300

    Adicionando a função de CHECK da quantidade de membros

--- database/tabelas - DDL/equipe.sql
+++ database/tabelas - DDL/equipe.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS equipe;
 CREATE TABLE equipe(
   id SERIAL PRIMARY KEY,
   nome VARCHAR(50) UNIQUE NOT NULL,
-  quantidade_membros INT NOT NULL,
+  quantidade_membros INT NOT NULL CHECK (quantidade_membros BETWEEN 5 AND 7),
   ativo BOOLEAN NOT NULL,
   vitorias INT NOT NULL,
   derrotas INT NOT NULL
