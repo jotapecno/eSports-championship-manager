4cadd342fd2611a0317eee4b5c6b8b4189012bba
Matheus Carvalho <157723585+Matheus-Barbosa01@users.noreply.github.com>
2026-09-08 14:10:55 -0300

    Atualizar o partida.sql
    foi adicionado as chaves estrangeiras

--- database/tabelas - DDL/partida.sql
+++ database/tabelas - DDL/partida.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS partida;
+
 CREATE TABLE partida (
     id SERIAL PRIMARY KEY,
     campeonato_id INT NOT NULL,
@@ -6,4 +8,9 @@ CREATE TABLE partida (
     equipe2_id INT NOT NULL,
     data_hora TIMESTAMP NOT NULL,
     local_partida VARCHAR(255) DEFAULT 'Online',
+
+FOREIGN KEY (campeonato_id) REFERENCES campeonato(id),
+    FOREIGN KEY (fase_id) REFERENCES fase(id),
+    FOREIGN KEY (equipe1_id) REFERENCES equipe(id),
+    FOREIGN KEY (equipe2_id) REFERENCES equipe(id)
 );
