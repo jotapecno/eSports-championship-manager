884166968a8f572920f2f63aaece16b42897bf01
Matheus Carvalho <157723585+Matheus-Barbosa01@users.noreply.github.com>
2026-09-13 20:30:13 -0300

    Update modalidade.sql
    comentarios para criação de tabelas

--- database/tabelas - DDL/modalidade.sql
+++ database/tabelas - DDL/modalidade.sql
@@ -1,5 +1,9 @@
 DROP TABLE IF EXISTS modalidade;
-
+-- ============================================================
+-- 1ª TABELA: modalidade
+-- Descrição: Registra os tipos de esportes ou jogos disputados.
+-- Dependências: Nenhuma (tabela base).
+-- ============================================================
 CREATE TABLE modalidade (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL UNIQUE,
